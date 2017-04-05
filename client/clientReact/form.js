b5227dbcfef113abb7b4bbae1c6cc1c249c6cac4
Nate <sauceynate@gmail.com>
2017-04-04 23:40:49 -0500

    getting the patch and update for restuarants written out, need to check on wether or not they work, moving right along

--- client/clientReact/form.js
+++ client/clientReact/form.js
@@ -2,18 +2,49 @@ var React = require('react');
 
 var FormComponent = React.createClass({
 	getInitialState: function(){
-		return {}
+		return {nameVal: '', menuVal: '', isTruck: false}
+	},
+	updateName: function(event){
+		var state = this.state;
+		state.nameVal = event.target.value
+		this.setState(state)
+	},
+	updateMenu: function(event){
+		var state = this.state;
+		state.menuVal = event.target.value
+		this.setState(state)
+	},
+	check: function(event){
+		var state = this.state;
+		state.isTruck = !state.isTruck;
+		this.setState(state)
+		
+	},
+	handleClick: function(event){
+		event.preventDefault();
+		this.props.onClickSubmit(this.state.nameVal)
+		this.props.onClickSubmit(this.state.menuVal)
+		this.props.onClickSubmit(this.state.isTruck)
+		//input reset
+		var state = this.state 
+		state.nameVal = '';
+		state.menuVAl = '';
+		state.isTruck = false;
+		this.setState(state)
 	},
 	render: function(){
+		console.log(this.state, ' this is this state')
 		return (
 			<div>
 				<form>
 					<input type="text" placeholder="Name" value={this.state.nameVal} onChange={this.updateName}/>
-					<label for="truck">Is it a Food Truck?</label><input id="truck" type="checkbox" name="truck"/>
+					<label for="truck">Is it a Food Truck?</label><input id="truck" type="checkbox" onChange={this.check} checked={this.state.isTruck} name="truck"/>
 					<input type="text" placeholder="Menu Link" value={this.state.menuVal} onChange={this.updateMenu}/>
-
-
-
+					<button onClick={this.handleClick}>Add Restaurant</button>
+				</form>
+			</div>
 		)
 	}
 })
+
+module.exports = FormComponent;
--- client/clientReact/main.js
+++ client/clientReact/main.js
@@ -6,12 +6,73 @@ var	request = require('superagent');
 var	FormComponent = require('./Form');
 
 var MainComponent = React.createClass({
-	getInitialState: function() {
-		return {}
+	getInitialState: function(){
+		return {data: [], modalFix: false, idToUpdate: '' }
+	},
+	componentDidMount: function(){
+		var state = this.state;
+		var self = this;
+		request.get('')
+			.end(function(err, data){
+				state.data = data.body;
+				self.setState(state);
+			})
+	},
+	createRestaurant: function(name, boolean, menu){
+		var state = this.state;
+		var self = this;
+		request.post('')
+			.type('form')
+			.send({name: name, truck: boolean, menu: menu})
+			.end(function(err, data){
+				console.log(data);
+				state.data = data.body;
+				self.setState(state);
+			})
+	},
+	update: function(id){
+		var state = this.state;
+		state.modalFix = true;
+		state.idToUpdate = id;
+		this.setState(state)
+	},
+	updateResturantVal: function(name, boolean, menu){
+		var state = this.state;
+		var self = this;
+		var objToSend = {
+			id: this.state.idToUpdate,
+			name: name,
+			truck: boolean,
+			menu: menu
+		}
+		request.patch('' + this.state.idToUpdate)
+			.type('form')
+			.send({
+				id: this.state.idToUpdate,
+				name: name,
+				truck: boolean,
+				menu: menu
+			})
+				.end(function(err, data){
+					console.log(data);
+					state.data = data.body;
+					self.setState(state)
+				})
 	},
 	render: function(){
+		var self = this;
 		return (
-			<h1>Hi</h1>
+			<div>
+				{this.state.data.map(function(item,I){
+					return(
+						<div>
+						</div>
+						)
+				})}
+
+
+				<FormComponent onClickSubmit=(this.createRestaurant)/>
+			</div>
 			)
 	}
 })
